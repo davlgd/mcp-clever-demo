6d06aee8ba23c1c86312bb981e84d87939ad8694
David Legrand <1110600+davlgd@users.noreply.github.com>
2025-01-20 09:31:15 +0100

    feat: add ressources

---
+++ src/ressources.js
@@ -0,0 +1,6 @@
+export const resources = {
+    docMarkdown: {
+        uri: 'https://www.clever-cloud.com/developers/llms.txt',
+        mimeType: 'text/markdown'
+    }
+};
--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 #! /usr/bin/env node
 
+import { resources } from './ressources.js';
 import { tools } from './tools.js';
 import { MCP } from 'mcp-js-server';
 
 const infos = {
     name: 'mcp-clever-demo',
-    version: '0.1.6'
+    version: '0.1.7'
 };
 
-const server = new MCP(infos, null, null, tools);
+const server = new MCP(infos, null, resources, tools);
